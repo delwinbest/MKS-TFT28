f6f8b835fb57989351de141f120254e96122974e
Delwin Best <mail@delwinbest.com>
2019-11-05 17:54:59 +0900

    Speaker beep test code

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,14 @@
 
 void setup() {
   // put your setup code here, to run once:
+  // initialize digital pin SPEAKER as an output.
+  pinMode(SPEAKER, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(SPEAKER, HIGH);   // turn the SPEAKER on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(SPEAKER, LOW);    // turn the SPEAKER off by making the voltage LOW
+  delay(1000); 
 }
