134d41317e2fdcd80f6164debe048159ad7b49b8
Delwin Best <mail@delwinbest.com>
2019-11-06 11:23:32 +0900

    aligned clock config to what was previously used.

--- buildroot/arduino_variant/MKSTFT_F107VC/variant.cpp
+++ buildroot/arduino_variant/MKSTFT_F107VC/variant.cpp
@@ -414,56 +414,55 @@ tmp=0x4;
 
 WEAK void SystemClock_Config(void)
 {
-    RCC_OscInitTypeDef RCC_OscInitStruct;
-    RCC_ClkInitTypeDef RCC_ClkInitStruct;
-#if defined(STM32F107xC) && defined(MKS_TFT)
-    RCC_PeriphCLKInitTypeDef PeriphClkInit;
-#endif
+    // RCC_OscInitTypeDef RCC_OscInitStruct;
+    // RCC_ClkInitTypeDef RCC_ClkInitStruct;
+    // RCC_PeriphCLKInitTypeDef PeriphClkInit;
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+    RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+
+    /** Initializes the CPU, AHB and APB busses clocks **/
     RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
     RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-#if defined(STM32F107xC) && defined(MKS_TFT)
     RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV5;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;  // Added from STMCube
     RCC_OscInitStruct.Prediv1Source = RCC_PREDIV1_SOURCE_PLL2;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE; 
+    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6; // Added from STMCube was RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
     RCC_OscInitStruct.PLL2.PLL2State = RCC_PLL2_ON;
     RCC_OscInitStruct.PLL2.PLL2MUL = RCC_PLL2_MUL8;
     RCC_OscInitStruct.PLL2.HSEPrediv2Value = RCC_HSE_PREDIV2_DIV5;
-#endif
-#if defined(STM32F103xE) && defined(CZMINI)
-    RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
-#endif
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
-    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-        Error_Handler();
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) // Added from STMCube
+    {
+      Error_Handler();
+    }
 
+    /** Initializes the CPU, AHB and APB busses clocks **/
     RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
             |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
     RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
     RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
     RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
     RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-        Error_Handler();
-
-#if defined(STM32F107xC) && defined(MKS_TFT)
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK) // Added from STMCube
+    {
+      Error_Handler();
+    }
     PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB;
-    PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL_DIV3;
+    PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL_DIV2; // Added from STMCube was PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL_DIV3;
     if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-        Error_Handler();
-#endif
+    {
+      Error_Handler();
+    }
 
-    HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
-    HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
-#if defined(STM32F107xC) && defined(MKS_TFT)
+    //HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000); // Not in STMCube
+    //HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK); // Not in STMCube
     __HAL_RCC_PLLI2S_ENABLE();
-#endif
-    HAL_NVIC_SetPriority(SysTick_IRQn, 15, 0);
+
+    //HAL_NVIC_SetPriority(SysTick_IRQn, 15, 0);  // Not in STMCube
 }
-
-
-
-
 
 
 void NMI_Handler(void)
