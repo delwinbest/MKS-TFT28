839e60fd139766ee47cdc0a10dcc39394575a7d6
Delwin Best <mail@delwinbest.com>
2019-12-08 17:01:53 +0900

    Correct build scripts, prep ini file

--- buildroot/scripts/postBuild.py
+++ buildroot/scripts/postBuild.py
@@ -1,18 +1,11 @@
-Import("env", "projenv")
-from shutil import copyfile
+Import("env")
+import shutil 
 print "Post build scripts"
 
-# access to global construction environment
-print(env)
-
-
 def after_build(source, target, env):
-    print "post_build_actions"
-    # do some actions
-    #print(env.Dump())
     source_filename = env['PROJECT_BUILD_DIR'] + "/" + env['PIOENV'] + "/" + env['PROGNAME'] + ".bin"
-    dst_filename = env['PROJECT_BUILD_DIR'] + "/buildroot/SDCARD/" + env['PROGNAME'] + ".bin"
-    copyfile(source_filename, dst_filename)
+    dst_filename = env['PROJECT_DIR'] + "/buildroot/SDCARD/" + env['PROGNAME'] + ".bin"
+    shutil.copyfile(source_filename, dst_filename)
     
-
 env.AddPostAction("buildprog", after_build)
+
